78aa5bc667c3687b246b9117939e657f9dbdcdc9
Irfan Maulana <mazipanneh@gmail.com>
2024-05-30 16:02:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,55 @@ const nextConfig = {
     appDir: true
   },
   poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },               
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'sameorigin',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, immutable, no-transform, max-age=31536000'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
