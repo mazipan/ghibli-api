a659152853585adaeb681835ed30f8bb922d83e8
Irfan Maulana <mazipanneh@gmail.com>
2024-05-31 06:05:34 +0700

    Update Meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,10 @@ display: 'swap', })
 export const metadata = {
   title: 'Ghibli API',
   description: 'Unofficial Ghibli Studio API',
-  favicon: "/favicon.ico"
+  favicon: "/favicon.ico",
+  other: {
+    'google-adsense-account': 'ca-pub-5442972248172818'
+  }
 }
 
 export default function RootLayout({
